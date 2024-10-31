013a53e77552fc6eb7dacc58ca994bc93c03bdd7
CTlandu <jizhoutang@outlook.com>
2024-10-31 16:33:19 -0400

    再次尝试。。。

--- run.py
+++ run.py
@@ -1,8 +1,14 @@
-from api import create_app
-from config import DevelopmentConfig, ProductionConfig
+from dotenv import load_dotenv
 import os
 
+# 根据环境加载对应的 .env 文件
 env = os.environ.get('FLASK_ENV', 'development')
+env_file = f'.env.{env}'
+load_dotenv(env_file)
+
+from api import create_app
+from config import DevelopmentConfig, ProductionConfig
+
 config = DevelopmentConfig if env == 'development' else ProductionConfig
 app = create_app(config)
 
