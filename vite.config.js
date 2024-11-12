36edc9bef1123fac4c2f465cb277d4b9c8682ab2
CTlandu <jizhoutang@outlook.com>
2024-11-12 02:16:06 -0500

    继续更新设置，主要集中在postcss和tailwindcss配置，来测试是否work

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
   plugins: [vue()],
@@ -9,6 +11,11 @@ export default defineConfig({
     assetsDir: 'assets',
     cssCodeSplit: true,
     sourcemap: true,
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
   },
   optimizeDeps: {
     include: ['tailwindcss', 'daisyui'],
